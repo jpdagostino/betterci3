d9af9cdbd63577610880bcd9784c0c2bc217fa4d
John-Paul D'Agostino <jp@jpdagostino.com>
2024-06-07 18:44:58 -0400

    Update Cache_redis.php

--- system/libraries/Cache/drivers/Cache_redis.php
+++ system/libraries/Cache/drivers/Cache_redis.php
@@ -224,7 +224,6 @@ class CI_Cache_redis extends CI_Driver
 		else
 		{
 			$this->_redis->expireAt($id, time() + $ttl);
-			$this->_redis->{static::$_sRemove_name}('_ci_redis_serialized', $id);
 		}
 
 		return TRUE;
